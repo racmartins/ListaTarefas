3806a9737fe730f5edef88f96d4842f600ea68fe
racmartins <rcmartins@outlook.pt>
2024-03-03 16:56:00 +0000

    Parte4
    Atribuição de tarefas a utilizadores.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Importar dependências
-const express = require("express"); //módulo express
+const express = require("express");
 const connection = require("./database/database"); //módulo de conexão
 
 // Criar uma instância do Express
@@ -11,79 +11,58 @@ app.set("view engine", "ejs");
 // Configurar o middleware para analisar dados de formulário
 app.use(express.urlencoded({ extended: false }));
 
-// Rota principal - exibir lista de tarefas
+// Rota principal - exibe a lista de tarefas
+/* 
+Nesta versão, estamos a usar um LEFT JOIN entre as tabelas tasks e users na BD. 
+A consulta agora retorna os dados da tabela tasks juntamente com o nome do utilizador 
+(como user_name) correspondente ao user_id na tabela users. De seguida, mapeamos os 
+resultados para criar o array de tarefas com as informações necessárias, 
+incluindo o nome do utilizador atribuído 
+*/
 app.get("/", (req, res) => {
-  // Consultar tarefas na base de dados
-  connection.query("SELECT * FROM tasks", (err, results) => {
-    if (err) throw err;
+  connection.query(
+    "SELECT tasks.*, users.name AS user_name FROM tasks LEFT JOIN users ON tasks.user_id = users.id",
+    (err, results) => {
+      if (err) throw err;
+      const tasks = results.map((task) => ({
+        id: task.id,
+        task: task.task,
+        status: task.status, //pendente, completa
+        user: {
+          id: task.user_id,
+          name: task.user_name,
+        },
+      }));
 
-    // Renderizar a página de lista de tarefas com os resultados da consulta
-    res.render("index", { tasks: results });
-  });
+      // Mostra a lista de utilizadores
+      connection.query("SELECT * FROM users", (err, users) => {
+        if (err) throw err;
+        res.render("index", { tasks, users });
+      });
+    }
+  );
 });
 
 // Rota para adicionar uma nova tarefa
 app.post("/add", (req, res) => {
-  const { user, task } = req.body;
-  // Inserir nova tarefa na base de dados
+  const { task, status, userId } = req.body;
   connection.query(
-    "INSERT INTO tasks (user, task) VALUES (?, ?)",
-    [user, task],
+    "INSERT INTO tasks (task, status, user_id) VALUES (?, ?, ?)",
+    [task, status, userId],
     (err, results) => {
       if (err) throw err;
-
-      // Redirecionar para a página principal
       res.redirect("/");
     }
   );
 });
-
-// Rota para exibir a página de edição da tarefa
-app.get("/edit/:id", (req, res) => {
-  const taskId = req.params.id;
-
-  // Consultar a tarefa com base no ID fornecido
+// Rota para associar uma tarefa a um utilizador
+app.post("/assign", (req, res) => {
+  const { userId, taskId } = req.body;
   connection.query(
-    "SELECT * FROM tasks WHERE id = ?",
-    [taskId],
+    "UPDATE tasks SET user_id = ? WHERE id = ?",
+    [userId, taskId],
     (err, results) => {
       if (err) throw err;
-
-      // Renderizar a página de edição da tarefa com os detalhes da tarefa
-      res.render("edit", { task: results[0] });
-    }
-  );
-});
-
-// Rota para atualizar a tarefa
-app.post("/update/:id", (req, res) => {
-  const taskId = req.params.id;
-  const { user, task } = req.body;
-
-  // Atualizar a tarefa na base de dados com base no ID fornecido
-  connection.query(
-    "UPDATE tasks SET user = ?, task = ? WHERE id = ?",
-    [user, task, taskId],
-    (err, results) => {
-      if (err) throw err;
-
-      // Redirecionar para a página principal após a atualização
-      res.redirect("/");
-    }
-  );
-});
-// Rota para remover a tarefa
-app.post("/delete/:id", (req, res) => {
-  const taskId = req.params.id;
-
-  // Remover a tarefa da base de dados com base no ID fornecido
-  connection.query(
-    "DELETE FROM tasks WHERE id = ?",
-    [taskId],
-    (err, results) => {
-      if (err) throw err;
-
-      // Redirecionar para a página principal após a remoção
       res.redirect("/");
     }
   );
